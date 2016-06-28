86c1cb5acd9a64ff415bb3b66af2051459f8a5f9
Tiffany Lam <lamtw@uci.edu>
2016-06-28 14:53:23 -0700

    Testing Tutorial
    hj

---
+++ Feed My Sheep/Assets/Scripts/TouchScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchScreen : MonoBehaviour {
+
+	void Update () {
+//Touch is a struct, not a class
+        Touch myTouch = Input.GetTouch(0);
+	}
+}
