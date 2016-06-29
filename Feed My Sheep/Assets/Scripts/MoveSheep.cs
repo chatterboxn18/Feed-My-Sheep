ec26b4eadb9e7f7396d30fe542ea81d0a0a2f9da
Tiffany Lam <lamtw@uci.edu>
2016-06-28 19:05:31 -0700

    A Test Run of Touch Screen

---
+++ Feed My Sheep/Assets/Scripts/MoveSheep.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSheep : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            float x = -4.5f + 9 * touch.position.x / Screen.width;
+            float y = -7.5f + 15 * touch.position.y / Screen.height;
+
+            transform.position = new Vector3(x, y, 0);
+        }
+	}
+}
--- Feed My Sheep/Assets/Scripts/TouchScreen.cs
+++ Feed My Sheep/Assets/Scripts/TouchScreen.cs
@@ -4,7 +4,21 @@ using System.Collections;
 public class TouchScreen : MonoBehaviour {
 
 	void Update () {
-//Touch is a struct, not a class
-        Touch myTouch = Input.GetTouch(0);
+//Touch is a struct, not a class 
 	}
+    void OnGUI()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            string message = "";
+            message += "ID: " + touch.fingerId + "\n";
+            message += "Phase: " + touch.phase.ToString() + "\n";
+            message += "TapCount: " + touch.tapCount + "\n";
+            message += "Pos X: " + touch.position.x + "\n";
+            message += "Pos Y: " + touch.position.y + "\n";
+
+            int num = touch.fingerId;
+            GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
+        }
+    }
 }
