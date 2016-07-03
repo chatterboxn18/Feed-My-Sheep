cf0e738d743ab46885e1468ad64e550b4906cb08
Tiffany Lam <lamtw@uci.edu>
2016-07-03 02:07:39 -0700

    Updated Sprite Sheet

--- Feed My Sheep/Assets/Scripts/MoveSheep.cs
+++ Feed My Sheep/Assets/Scripts/MoveSheep.cs
@@ -9,10 +9,13 @@ public class MoveSheep : MonoBehaviour {
         {
             Touch touch = Input.GetTouch(0);
 
-            float x = -4.5f + 9 * touch.position.x / Screen.width;
-            float y = -7.5f + 15 * touch.position.y / Screen.height;
-
+            float x = -3.5f + 7 * touch.position.x / Screen.width;
+            float y = -5.0f + 10 * touch.position.y / Screen.height;
             transform.position = new Vector3(x, y, 0);
+            string message = "X: " + touch.position.x + " Y:" + touch.position.y;
+            GUI.Label(new Rect(0, 0, 120, 100), message);
+            
         }
+
 	}
 }
